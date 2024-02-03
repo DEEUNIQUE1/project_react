286a51e2eed8e045ba56bf0261dfcbef641c411a
Samuel Oyimafu Victor <soyimafu@inlaks.com>
2024-02-03 05:03:15 +0100

    project done

---
+++ src/Data.jsx
@@ -0,0 +1,47 @@
+const Data = [
+    {
+        id: 1,
+        title: 'Iphone 11',
+        price: 750,
+        desc : ' lorem ipsum dolor sit amet, consectetur adip, sed do eiusmod tempor incididunt ut lab',
+        img : '../assets/images/products/iphone11promax.png'
+    },
+    {
+        id: 2,
+        title: 'Iphone 11 pro max',
+        price: 800,
+        desc : ' lorem ipsum dolor sit amet, consectetur adip, sed do eiusmod tempor incididunt ut lab',
+        img : '../assets/images/products/iphone11promax.png'
+    },
+    {
+        id: 3,
+        title: 'Iphone 12',
+        price: 750,
+        desc : ' lorem ipsum dolor sit amet, consectetur adip, sed do eiusmod tempor incididunt ut lab',
+        img : '../assets/images/products/iphone12promax.png'
+    },
+    {
+        id: 4,
+        title: 'Iphone 12 mini',
+        price: 850,
+        desc : ' lorem ipsum dolor sit amet, consectetur adip, sed do eiusmod tempor incididunt ut lab',
+        img : '../assets/images/products/iphone12mini.png'
+    },
+    {
+        id: 5,
+        title: 'Iphone 12 pro',
+        price: 900,
+        desc : ' lorem ipsum dolor sit amet, consectetur adip, sed do eiusmod tempor incididunt ut lab',
+        img : '../assets/images/products/iphone12pro.png'
+    },
+    {
+        id: 6,
+        title: 'Iphone 12pro max',
+        price: 950,
+        desc : ' lorem ipsum dolor sit amet, consectetur adip, sed do eiusmod tempor incididunt ut lab',
+        img : '../assets/images/products/iphone12promax.png'
+    }
+
+]
+
+export default Data
